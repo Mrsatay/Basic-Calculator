e41781d030a6e1fc56a7ba715bae7775f95caa2f
Maulana <maulanadwicahyana@gmail.com>
2023-04-12 07:53:28 +0700

    Adding number two

---
+++ DataType.py
@@ -0,0 +1,37 @@
+# tipe data Boolean
+print(True)
+print(False)
+
+# tipe data String
+print("ayo belajar python")
+print('bELAJAR DS SANGAT MUDAH')
+
+# tipe data Integer
+print(20)
+print(200)
+
+# tipe data Float
+print(3.14)
+print(3.1434534)
+
+# tipe data Hexadecimal
+print(0x9a)
+
+# tipe data Complex
+print(5j)
+
+# tipe data list
+print([1,2,3,4,5])
+print(['satu','dua','tiga'])
+
+# tipe data Tuple
+print((1,2,3,4,5))
+print(('satu','dua','tiga'))
+
+# tipe data Dictionary
+print({'nama':'Budi','umur':20})
+
+# tipe data Dictionary dimasukan ke dalam variable biodata
+biodata = {'nama':'andi','umur':20} # proses insialisasi variable biodata
+print(biodata) # proses pencetakan variabel biodata yang berisi tipe data Dictionary
+print(type(biodata)) # Fungsi untuk mengecek jenis tipe data. akan tampil <class 'dict'> yang berarrti dict adalah tipe data dictionary
