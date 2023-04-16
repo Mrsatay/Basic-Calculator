3209488713faca81a95b5a0d33a8f1c626be5a34
Mrsatay <108076719+Mrsatay@users.noreply.github.com>
2023-04-16 21:51:30 +0700

    Adding some point or continue module

--- DataStrucutre.py
+++ DataStrucutre.py
@@ -41,27 +41,21 @@ print(calculate_median(a))
 
 # 3. Buatlah sebuah fungsi yang dapat menghitung modus dari sebuah vektor numerik.
 
-# def calculate_modus(num):
+def calculate_modus(num):
     
-#     mode = num[0]
+    mode = num[0]
 
-#     for i in range(len(num) - 1):
+    for i in range(len(num) - 1):
         
-#         if np.count_nonzero(num == mode) < np.count_nonzero(num == num[i + 1]):
-#             mode = num[i + 1]
-#         else:
-#             pass
+        if np.count_nonzero(num == mode) < np.count_nonzero(num == num[i + 1]):
+            mode = num[i + 1]
+        else:
+            pass
 
-#     return f'from {num} \nmode is: {mode}'
+    return f'from {num} \nmode is: {mode}'
     
-<<<<<<< HEAD
-# a = np.array([random.randint(1,30) for i in range(10)])
-# print(calculate_modus(a))   
-=======
 a = np.array([random.randint(1,30) for i in range(10)])
 print(calculate_modus(a))   
-
->>>>>>> be4242a0bdb99b627d9036e047db9906baac5def
     # this code it's not done yet because it just done for show 1 mode value, but
     # what if there is more than one mode value?
 
@@ -69,63 +63,63 @@ print(calculate_modus(a))
 
 # 4. Buatlah sebuah fungsi yang dapat menghitung variance dari sebuah vektor numerik.
 
-# def calculate_variance_singleData(num):
+def calculate_variance_singleData(num):
 
-#     avg = np.mean(num)
-#     res = 0
-#     n = len(num)
+    avg = np.mean(num)
+    res = 0
+    n = len(num)
 
-#     for i in range(n):
-#         res = (num[i] - avg) ** 2
+    for i in range(n):
+        res = (num[i] - avg) ** 2
     
-#     return f'from {num} \nvariance single data is: {round(res/n,2)}'
+    return f'from {num} \nvariance single data is: {round(res/n,2)}'
 
-# a = np.array([random.randint(1,30) for i in range(10)])
-# print(calculate_variance_singleData(a))
+a = np.array([random.randint(1,30) for i in range(10)])
+print(calculate_variance_singleData(a))
 
 
 # 5. Buatlah sebuah fungsi yang dapat menghitung standard deviation dari sebuah vektor numerik.
 
-# def calculate_standard_deviation(num):
+def calculate_standard_deviation(num):
 
-#     avg = np.mean(num)
-#     res = 0
-#     n = len(num)
+    avg = np.mean(num)
+    res = 0
+    n = len(num)
 
-#     for i in range(n):
-#         res = (num[i] - avg) ** 2
+    for i in range(n):
+        res = (num[i] - avg) ** 2
 
-#     std = round(math.sqrt(res/n),2)
+    std = round(math.sqrt(res/n),2)
 
-#     return f'from {num} \nstandard deviation is: {std}'
+    return f'from {num} \nstandard deviation is: {std}'
 
-# a = np.array([random.randint(1,30) for i in range(10)])
-# print(calculate_standard_deviation(a))
+a = np.array([random.randint(1,30) for i in range(10)])
+print(calculate_standard_deviation(a))
 
 # 6. Buatlah sebuah fungsi yang dapat menghitung covariance matrix dari dua vektor numerik.
 
-# def covariance_matrix(num1,num2):
+def covariance_matrix(num1,num2):
 
-#     avg_x = np.mean(num1)
-#     avg_y = np.mean(num2)   
-#     Xi_Xavg = []
-#     Yi_Yavg = []
-#     product = []
-#     n = len(num1)
+    avg_x = np.mean(num1)
+    avg_y = np.mean(num2)   
+    Xi_Xavg = []
+    Yi_Yavg = []
+    product = []
+    n = len(num1)
 
-#     for i in range(n):
-#         Xi_Xavg.append(num1[i] - avg_x)
-#         Yi_Yavg.append(num2[i] - avg_y)
-#         product.append(Xi_Xavg[i] * Yi_Yavg[i])
+    for i in range(n):
+        Xi_Xavg.append(num1[i] - avg_x)
+        Yi_Yavg.append(num2[i] - avg_y)
+        product.append(Xi_Xavg[i] * Yi_Yavg[i])
 
-#     product = np.array(product)
-#     covariance = round(product.sum() / (n - 1),2)
-#     return f'from\t{num1}\n\t{num2} \nCovariance is: {covariance}'
+    product = np.array(product)
+    covariance = round(product.sum() / (n - 1),2)
+    return f'from\t{num1}\n\t{num2} \nCovariance is: {covariance}'
 
 
-# a = np.array([random.randint(1,30) for i in range(10)])
-# b = np.array([random.randint(1,30) for i in range(10)])
-# print(covariance_matrix(a,b))
+a = np.array([random.randint(1,30) for i in range(10)])
+b = np.array([random.randint(1,30) for i in range(10)])
+print(covariance_matrix(a,b))
 
 # 7. Buatlah sebuah fungsi yang dapat menghitung correlation matrix dari dua vektor numerik.
 
